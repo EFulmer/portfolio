0ad9c228b89dff1046bd5b397aec92d9d36c483d
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2025-05-18 12:34:39 -0400

    ENH: add To-Do List page

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('about', views.about, name='about'),
+    path('todo', views.todo, name='todo'),
 ]
