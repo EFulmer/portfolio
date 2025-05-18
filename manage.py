1eea7d625e8fed11bdcbec843f9a57beea832477
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2025-05-18 14:10:50 -0400

    ENH: add python-dotenv for more secure secret management

--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+import dotenv
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), 'env.list'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
