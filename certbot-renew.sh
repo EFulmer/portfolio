2e2200d231b6d4791d7b3dfae0303db670fa3f54
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2026-06-28 01:32:16 +0000

    BUG: nginx not reloading on certbot refresh

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,11 +1,12 @@
-#!/bin/sh -u
+#!/bin/sh
 # certbot-renew.sh
 # Safe loop to renew certificates every 12 hours
 
+trap exit TERM
+
 while true; do
-    certbot renew \
-        --webroot -w /var/www/certbot \
-        --verbose \
-        --deploy-hook "echo 'Certificate renewed!'"
-    sleep 12h
+    echo "Running certbot renew..."
+    certbot renew --webroot -w /var/www/certbot --verbose --deploy-hook "echo 'Certificate renewed!'"
+    sleep 12h &
+    wait $!
 done
